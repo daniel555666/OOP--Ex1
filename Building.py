b6eacc292ca09f0e7c30320223c522568c438154
dvirGev <dvir87879@gmail.com>
2021-11-08 17:03:49 +0200

    commit

--- Building.py
+++ Building.py
@@ -1,9 +1,18 @@
+import json
 from Elevators import Elevators
 
 
 class Building:
-      def __init__(self):
+    def __init__(self):
           self._minFloor = 0
           self._maxFloor = 10
           self._elevators = [Elevators(), Elevators(), Elevators]
           print("hello")
+    def __init__(self, file_name):
+          self.from_json(self, file_name)
+    def from_json(self,file_name):
+        with open(file_name,"r") as fp:
+            di=json.load(fp)
+            self._minFloor=di["_minFloor"]
+            self._maxFloor=di["_maxFloor"]
+            self._elevators=di["_elevators"]
