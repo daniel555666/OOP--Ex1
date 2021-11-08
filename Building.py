346a1f96a65c44a37877c4e5de3152e046c14942
daniel555666 <danielzaken5@gmail.com>
2021-11-08 17:02:10 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	test.py

--- Building.py
+++ Building.py
@@ -2,8 +2,10 @@ from Elevators import Elevators
 
 
 class Building:
-      def __init__(self):
-          self._minFloor = 0
-          self._maxFloor = 10
-          self._elevators = [Elevators(), Elevators(), Elevators]
-          print("hello")
+    def __init__(self):
+        self._minFloor = 0
+        self._maxFloor = 10
+        self._elevators = [Elevators(), Elevators(), Elevators]
+        print("hello")
+    def __print(self):
+        print("hello")
