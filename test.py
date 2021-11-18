9e98932fca57cf1f59c33e53943ff322e0498624
dvirGev <dvir87879@gmail.com>
2021-11-18 15:51:26 +0200

    commit

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import subprocess
 class TestStringMethods(unittest.TestCase):
     def test_code(self):
         subprocess.Popen(["powershell.exe", "python Ex1.py input\Ex1_Buildings\B3.json input\Ex1_Calls\Calls_a.csv myOutput.csv"])
-        subprocess.Popen(["powershell.exe", "java -jar lib\Ex1_checker_V1.2_obf.jar 207296989,209530583 " +
-                     "input\Ex1_Buildings\B3.json" + "  " + "myOutput.csv" + "  outputFormTEster.log >testResults.txt"])
+        
+        subprocess.Popen(["powershell.exe", "java -jar lib\Ex1_checker_V1.2_obf.jar 207296989,209530583 " + "input\Ex1_Buildings\B3.json" + "  " + "myOutput.csv" + "  outputFormTEster.log >testResults.txt"])
         
         with open('expectResults.txt') as f:
             exp = f.read()
